bc5d49ac622187a7f130310f020819734116ba06
uakbas <uveyis.akbas@outlook.com>
2024-08-03 14:32:24 +0300

    Load binary masks

--- scene.py
+++ scene.py
@@ -12,7 +12,9 @@ cameras = {
     'bottom': Camera(getRotMatX(90), torch.tensor([0, radius, 0], dtype=torch.float32)),
 }
 
-image_dir = 'images'
+scene_dir = 'scenes/scene_1'
+
+image_dir = os.path.join(scene_dir, 'images')
 image_names = ['front', 'top', 'back', 'bottom']
 images = {}
 for name in image_names:
@@ -23,3 +25,19 @@ for name in image_names:
         continue
 
     images[name] = image
+
+mask_dir = os.path.join(scene_dir, 'masks')
+mask_names = image_names
+masks = {}
+
+for name in mask_names:
+    mask_path = os.path.join(mask_dir, f'{name}.png')
+    mask = cv.imread(mask_path, cv.IMREAD_GRAYSCALE)
+    _, mask_binary = cv.threshold(mask, 0, 255, cv.THRESH_BINARY)
+    mask_binary = mask_binary / 255
+
+    if mask is None:
+        print(f'Could not read mask {mask_path}')
+        continue
+
+    masks[name] = mask_binary
